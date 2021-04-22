d34a0dadbdbcf872d846134c8bb90c178f650956
y2sang <37392688+y2sang@users.noreply.github.com>
2021-04-22 11:09:41 +0900

    .

---
+++ clima_flutter/android/app/src/main/kotlin/com/sang2/clima_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sang2.clima_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
