f4191cc6690b583c53888f79cafd3af1eefe5db4
y2sang <37392688+y2sang@users.noreply.github.com>
2021-12-18 13:27:29 +0900

    .

---
+++ fooderlich_flutter/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich_flutter/home.dart';
+
+import 'fooderlich_theme.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create theme
+    final theme = FooderlichTheme.dark();
+    // TODO: Apply Home widget
+    // 3
+    return MaterialApp(
+      // TODO: Add theme
+      theme: theme,
+      title: 'Fooderlich',
+      // 4
+      home: const Home(),
+    );
+  }
+}
